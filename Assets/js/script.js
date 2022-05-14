9896d6d16c8025dbc66e9d9c4a8459b78c7b8931
Eusebiu Ciobanu <sebiu.ciobanu98@gmail.com>
2022-05-14 17:25:21 +0200

    Commit con operazioni

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -47,30 +47,61 @@ function numZero() {
     document.getElementById('risultato').value += 0;
 }
 
-let op1
+function cancella() {
+
+    document.getElementById('risultato').value = ' '
+}
+
+let op1;
 function addizione(){
      num1 = +(document.getElementById('risultato').value);
-    document.getElementById('risultato').value += '+'
+    document.getElementById('risultato').value += '+';
     
 }
 
-let op2
+let op2;
 function sottrazione() {
     op2 = +(document.getElementById('risultato').value);
-    document.getElementById('risultato').value += '-'  
+    document.getElementById('risultato').value += '-';
     
 }
 
-let op3
+let op3;
 function moltiplicazione() {
     op3 = +(document.getElementById('risultato').value);
-    document.getElementById('risultato').value += 'x'
+    document.getElementById('risultato').value += 'x';
    
 }
 
-let op4
+let op4;
 function divisione() {
     op4 = +(document.getElementById('risultato').value);
-    document.getElementById('risultato').value += '/'
+    document.getElementById('risultato').value += '/';
     
+}
+
+let numFinale;
+function operazione() {
+    numFinale = document.getElementById('risultato').value;
+    if (numFinale.includes('+')) {
+        let somma = numFinale.split('+');
+        risultato = +somma[0] + +somma[1]; 
+        document.getElementById('risultato').value = risultato;
+    } else 
+    if (numFinale.includes('-')) {
+        let divis = numFinale.split('-');
+        risultato = +divis[0] - +divis[1]; 
+        document.getElementById('risultato').value = risultato;
+    } else 
+    if (numFinale.includes('x')) {
+        let molti = numFinale.split('x');
+        risultato = +molti[0] * +molti[1]; 
+        document.getElementById('risultato').value = risultato;
+    }
+    else 
+    if (numFinale.includes('/')) {
+        let diviso = numFinale.split('/');
+        risultato = +diviso[0] / +diviso[1]; 
+        document.getElementById('risultato').value = risultato;
+    }
 }
